faecac063f9377ebeb734ba956c40496c92ac164
ezge <ezge.rea@gmail.com>
2025-05-09 13:32:35 +0200

    Add dependency injection to GetCartItemsUseCase and PlaceOrderUseCase

--- mvvm/src/main/java/com/example/storeapp/domain/usecase/GetCartItemsUseCase.kt
+++ mvvm/src/main/java/com/example/storeapp/domain/usecase/GetCartItemsUseCase.kt
@@ -2,9 +2,10 @@ package com.example.storeapp.domain.usecase
 
 import com.example.storeapp.domain.model.CartItem
 import com.example.storeapp.domain.repository.CartRepository
+import javax.inject.Inject
 
 
-class GetCartItemsUseCase(private val cartRepository: CartRepository) {
+class GetCartItemsUseCase @Inject constructor(private val cartRepository: CartRepository) {
     operator fun invoke(): List<CartItem> {
         return cartRepository.getCartItems()
     }
--- mvvm/src/main/java/com/example/storeapp/domain/usecase/PlaceOrderUseCase.kt
+++ mvvm/src/main/java/com/example/storeapp/domain/usecase/PlaceOrderUseCase.kt
@@ -2,8 +2,9 @@ package com.example.storeapp.domain.usecase
 
 import com.example.storeapp.domain.model.Order
 import com.example.storeapp.domain.repository.OrderRepository
+import javax.inject.Inject
 
-class PlaceOrderUseCase(private val orderRepository: OrderRepository) {
+class PlaceOrderUseCase @Inject constructor(private val orderRepository: OrderRepository) {
     suspend operator fun invoke(order: Order): Boolean {
         return orderRepository.placeOrder(order)
     }
